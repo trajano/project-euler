87154633bbfdb7c00fa5dad135354c4858c08d78
Archimedes Trajano <arch@trajano.net>
2016-01-04 01:43:19 -0500

    Changed to use GregorianCalendar
    Makes the clode slightly more readable

--- java/E019.java
+++ java/E019.java
@@ -1,24 +1,18 @@
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import static java.util.Calendar.*;
 
 public class E019 {
     public static void main(final String[] argv) {
-        final Calendar c = Calendar.getInstance();
-        c.set(MONTH, JANUARY);
-        c.set(DAY_OF_MONTH, 1);
-        c.set(YEAR, 1901);
-
-        final Calendar limit = Calendar.getInstance();
-        limit.set(MONTH, DECEMBER);
-        limit.set(DAY_OF_MONTH, 31);
-        limit.set(YEAR, 2000);
+        final Calendar c = new GregorianCalendar(1901, JANUARY, 1);
+        final Calendar limit = new GregorianCalendar(2000, DECEMBER, 31);
 
         int count = 0;
         while (c.before(limit)) {
-        	if (c.get(DAY_OF_WEEK) == SUNDAY) {
-        		++count;
-        	}
-        	c.add(MONTH, 1);
+           if (c.get(DAY_OF_WEEK) == SUNDAY) {
+                ++count;
+           }
+           c.add(MONTH, 1);
         }
         System.out.println(count);
     }
