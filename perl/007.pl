bb4bdf1ee760189f5fc0ac7553b4f26c7e044473
Archimedes Trajano <arch@trajano.net>
2016-01-04 00:58:27 -0500

    Faster version 002
    Evaluating against each known prime seems slower

--- perl/007.pl
+++ perl/007.pl
@@ -5,20 +5,36 @@ my %known_primes_hash = map { $_ => 1 } @known_primes;
 my $max_known_prime = 13;
 my $n = 13;
 
-sub add_if_prime() {
-    foreach my $prime (keys %known_primes_hash) {
-        if ($n % $prime == 0) {
-            return false;
-        }
+sub add_if_prime2() {
+    if (is_prime($n)) {
+        print "$n is prime\n";
+        push @known_primes, $n;
+        $known_primes_hash{$n} = 1;
+        $max_known_prime = $n;
     }
-    push @known_primes, $n;
-    $known_primes_hash{$n} = 1;
-    $max_known_prime = $n;
-    return true;
 }
+
+sub is_prime($) {
+    my $x = shift;
+    if ($x <= 1) {
+        return 0;
+    } elsif ($x <= 3) {
+        return 1;
+    } elsif (($x % 2) == 0 or ($x % 3) == 0 or ($x % 5) == 0) {
+        return 0;
+    }
+    my $i = 5;
+    while (($i * $i) <= $x) {
+        return 0 if ($x % $i) == 0;
+        return 0 if ($x % ($i + 2)) == 0;
+        $i += 6;
+    }
+    return 1;
+}
+
 while (0 + @known_primes < 10001) {
     $n += 2;
     
-    add_if_prime();
+    add_if_prime2();
 }
 print $max_known_prime;
