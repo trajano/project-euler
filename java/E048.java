8f3a01d970d88b83892297e00023e5c01f51d30f
Archimedes Trajano <arch@trajano.net>
2016-01-08 01:26:18 -0500

    Only trim if the multiplication will make it go over cap

--- java/E048.java
+++ java/E048.java
@@ -3,12 +3,13 @@ import java.math.*;
 public class E048 {
     public static void main(final String[] argv) {
     	
+    	final long cap = Long.MAX_VALUE / 1000;
     	long c = 0;
     	for (long i = 1; i <= 1000; ++i) {
     		long a = 1;
     		for (int j = 0; j < i; ++j) {
     			a *= i;
-    			if (a > 10000000000000L) {
+    			if (a > cap) {
     				final String ta = String.valueOf(a);
     				a  = Long.valueOf(ta.substring(ta.length() - 10));
     			}
