f47e4303205ad3ba63c578900435d28d66070034
Archimedes Trajano <arch@trajano.net>
2016-01-08 01:58:47 -0500

    Added 006 in Java

---
+++ java/E006.java
@@ -0,0 +1,21 @@
+import java.math.*;
+
+public class E006 {
+    public static void main(final String[] argv) {
+    	
+
+    	BigInteger c = BigInteger.ZERO;
+    	for (int i = 1; i <= 100; ++i) {
+    		BigInteger a = new BigInteger(String.valueOf(i)).pow(2);
+    		c = c.add(a);
+    	}
+
+        int d = 0;
+        for (int i = 1; i <= 100; ++i) {
+            d += i;
+        }
+        BigInteger e = new BigInteger(String.valueOf(d)).pow(2);
+
+        System.out.println(e.subtract(c));
+    }
+}
