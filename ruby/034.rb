630bbf75edaa7b65b9752f338d8ca57deff3c979
Archimedes Trajano <arch@trajano.net>
2016-01-09 23:03:39 -0500

    Shortened factorial caching

--- ruby/034.rb
+++ ruby/034.rb
@@ -1,23 +1,7 @@
-def fact(n)
-   if (n == 0)
-       return 1
-   else 
-      return n * fact(n-1);
-   end
+$facts = [1]
+(1..9).each do |n|
+  $facts[n] = n * $facts[n-1]
 end
-
-$facts = [
-  fact(0),
-  fact(1),
-  fact(2),
-  fact(3),
-  fact(4),
-  fact(5),
-  fact(6),
-  fact(7),
-  fact(8),
-  fact(9)
-]
 
 def sum_factorial_of_digits(n)
   a = n.to_s.chars.map(&:to_i)
