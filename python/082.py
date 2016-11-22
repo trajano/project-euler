d65a685a1c628d68390ecead5ad0876be526ad39
Archimedes Trajano <arch@trajano.net>
2016-11-22 16:32:16 -0500

    082 still not working

--- python/082.py
+++ python/082.py
@@ -1,4 +1,5 @@
 import re
+import networkx
 from urllib.request import urlopen
 
 link = 'https://projecteuler.net/project/resources/p082_matrix.txt'
@@ -6,11 +7,11 @@ f = urlopen(link)
 input_matrix = [[int(e) for e in l.decode().split(",")] for l in f.readlines()]
 
 xinput_matrix = [
-    [537, 699, 497, 121, 956],
-    [805, 732, 524, 37, 331],
     [131, 673, 234, 103, 18],
     [201, 96, 342, 965, 150],
     [630, 803, 746, 422, 111],
+    [537, 699, 497, 121, 956],
+    [805, 732, 524, 37, 331],
 ]
 
 
@@ -78,46 +79,61 @@ def three_way_minimal_path_sum(input_matrix, x, y):
 minpath_matrix = None
 
 
-def build_minpath_matrix(input_matrix):
+def seed_minpath_matrix(input_matrix):
     global minpath_matrix
-    minpath_matrix = [[-1 for x in range(len(input_matrix))]
+    w = len(input_matrix)
+    minpath_matrix = [[-1 for x in range(0, w)]
                       for y in range(len(input_matrix))]
-    for w in range(len(input_matrix) - 1, -1, -1):
-        for j in range(0, len(input_matrix)):
-            calculate_minpath(input_matrix, minpath_matrix, w, j)
+    for i in range(0, w):
+        minpath_matrix[i][w - 1] = input_matrix[i][w - 1]
 
 
 def calculate_minpath(input_matrix, minpath_matrix, i, j):
     w = len(input_matrix)
     if (minpath_matrix[i][j] != -1):
         return minpath_matrix[i][j]
-    minpath_matrix[i][j] = 99999999
+    if (minpath_matrix[i][j] == -2):
+        return 9999999
+    minpath_matrix[i][j] = -2
     if (j == w - 1):
         minpath_matrix[i][j] = input_matrix[i][j]
 
-    elif (i < w - 1) and (i > 0):
-        minpath_matrix[i][j] = input_matrix[i][j] + min(
-            calculate_minpath(input_matrix, minpath_matrix, i, j + 1),
-            calculate_minpath(input_matrix, minpath_matrix, i - 1, j),
-            calculate_minpath(input_matrix, minpath_matrix, i + 1, j))
     elif (i == 0):
+        #print("Z")
         minpath_matrix[i][j] = input_matrix[i][j] + min(
             calculate_minpath(input_matrix, minpath_matrix, i, j + 1),
             calculate_minpath(input_matrix, minpath_matrix, i + 1, j))
     elif (i == w - 1):
+        #print("Y")
         minpath_matrix[i][j] = input_matrix[i][j] + min(
-            calculate_minpath(input_matrix, minpath_matrix, i, j + 1),
-            calculate_minpath(input_matrix, minpath_matrix, i - 1, j))
+            calculate_minpath(input_matrix, minpath_matrix, i - 1, j),
+            calculate_minpath(input_matrix, minpath_matrix, i, j + 1))
     else:
-        throw
+        #print("X" + str([i, j, w]))
+        minpath_matrix[i][j] = input_matrix[i][j] + min(
+            min(
+                calculate_minpath(input_matrix, minpath_matrix, i, j + 1),
+                calculate_minpath(input_matrix, minpath_matrix, i - 1, j),
+                calculate_minpath(input_matrix, minpath_matrix, i + 1, j)),
+            min(
+                calculate_minpath(input_matrix, minpath_matrix, i - 1, j),
+                calculate_minpath(input_matrix, minpath_matrix, i + 1, j),
+                calculate_minpath(input_matrix, minpath_matrix, i, j + 1)),
+            min(
+                calculate_minpath(input_matrix, minpath_matrix, i + 1, j),
+                calculate_minpath(input_matrix, minpath_matrix, i - 1, j),
+                calculate_minpath(input_matrix, minpath_matrix, i, j + 1)))
+    print([i, j, minpath_matrix[i][j]])
     return minpath_matrix[i][j]
-
-
-build_minpath_matrix(input_matrix)
 
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
 
+seed_minpath_matrix(xinput_matrix)
+calculate_minpath(xinput_matrix, minpath_matrix, 0, 0)
+
 print(min(column(minpath_matrix, 0)))
+for x in minpath_matrix:
+    print(x)
